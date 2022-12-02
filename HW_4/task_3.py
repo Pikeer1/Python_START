d4e19d614e4d6f95a9acd1df488a872ad22b9ecc
Paul <f1rstmarious@yandex.ru>
2022-12-02 20:47:49 +0300

    task 3 completed

--- HW_4/task_3.py
+++ HW_4/task_3.py
@@ -10,3 +10,18 @@
 2
 2x^2 + 4x + 5 = 0
 """
+
+from random import randint
+
+max_value = 100
+k = int(input('Введите коэффициент: '))
+koef = [randint(0, max_value) for i in range(k)] + [randint(1, max_value)]
+polynom = ' + '.join([f'{(j, "")[j == 1]}x^{i}' for i, j in enumerate(koef) if j][::-1])
+polynom = polynom.replace('x^1+', 'x+')
+polynom = polynom.replace('x^0', '')
+polynom += ('', '1')[polynom[-1] == '+']
+polynom = (polynom, polynom[:-2])[polynom[-2:] == '^1']
+print(polynom)
+
+with open('task3.txt', 'w') as data:
+    data.write(polynom)
