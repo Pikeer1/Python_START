ffe7f3b6a60608bc745910e85e9629cf1f41ccbc
Paul <f1rstmarious@yandex.ru>
2022-12-02 20:28:02 +0300

    task 2 completed

--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -8,3 +8,14 @@
 [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 [2, 4, 6, 8]
 """
+
+from random import randint
+
+number = int(input("Задайте размерность списка: "))
+first_list = [randint(1, 8) for i in range(number)]
+second_list = []
+for i in first_list:
+    if first_list.count(i) == 1:
+        second_list.append(i)
+print(f"Исходный список: {first_list}")
+print(f"Список из неповторяющихся элементов: {second_list}")
