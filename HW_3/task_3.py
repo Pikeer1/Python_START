cded67f79af9c79a3621ae6eab5af01ef7e2064d
Paul <f1rstmarious@yandex.ru>
2022-12-02 18:53:39 +0300

    task 3 completed

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -10,3 +10,13 @@
 2.0
 """
 
+import random
+
+number = int(input("Задайте размерность списка: "))
+my_list = [round(random.uniform(1, 5), 2) for i in range(number)]
+list_2 = []
+for i in range(len(my_list)):
+    list_2.append(my_list[i] - int(my_list[i]))
+result = max(list_2) - min(list_2)
+print(f"Сгенерированный список: {my_list}")
+print(f"Результат: {round(result, 2)}")
