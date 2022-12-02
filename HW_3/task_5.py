4c14c4a7286924597cab18b5d2fffeb3c6b2cfa7
Paul <f1rstmarious@yandex.ru>
2022-12-02 18:57:32 +0300

    task 5 completed

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -11,3 +11,10 @@ https://ru.wikipedia.org/wiki/Негафибоначчи
 [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 """
 
+num = int(input("Введите целое число: "))
+new_list = [0 for i in range(num * 2 + 1)]
+new_list[num + 1] = new_list[num - 1] = 1
+for i in range(num - 1):
+    new_list[num + 2 + i] = new_list[num + 1 + i] + new_list[num + i]
+    new_list[num - 2 - i] = new_list[num + 2 + i] * ((-1) ** (i + 1))
+print(f"Результат: {new_list}")
