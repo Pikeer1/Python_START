0fabf1f6f0bdd93b38b5f1759193d3c377f28e91
Paul <f1rstmarious@yandex.ru>
2022-12-15 20:02:31 +0300

    task 2 completed

--- HW_6/task_2.py
+++ HW_6/task_2.py
@@ -13,3 +13,22 @@
 [1, 3, 5]
 [1, 2, 5, 3, 10]
 """
+
+from random import randint
+
+lst = [randint(1, 10) for i in range(6)]
+lst_1 = []
+lst_2 = []
+for num in lst:
+    if lst.count(num) == 1:
+        lst_1.append(num)
+    else:
+        if lst_2.count(num) == 0:
+            lst_2.append(num)
+
+lst_3 = list(set(lst))
+
+print(f"Исходный список: {lst}")
+print(f"Неповторяющиеся числа: {lst_1}")
+print(f"Повторяющиеся числа: {lst_2}")
+print(f"Список без повторений: {lst_3}")
