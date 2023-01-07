ccbb0ce58ae16107c03285528796ed9b4ed5b9ca
Paul <f1rstmarious@yandex.ru>
2023-01-07 17:02:07 +0300

    sorry - revert last 3 commits because I was not careful

--- HW_10/phonebook/controller.py
+++ HW_10/phonebook/controller.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
 def start():
-    my_token = "input token here"
+    my_token = "5807651404:AAHL2ysrkFqlODCmk_snxneYfKQtizr3D1g"
     app = ApplicationBuilder().token(my_token).build()
     app.add_handler(CommandHandler("start", view.greetings))
     app.add_handler(CommandHandler("show", view.show_book))
