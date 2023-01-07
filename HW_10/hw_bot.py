01dccd8281640b676c054feddcef8b8d9df60f27
Paul <f1rstmarious@yandex.ru>
2023-01-07 17:15:55 +0300

    change

--- HW_10/hw_bot.py
+++ HW_10/hw_bot.py
@@ -18,7 +18,7 @@ async def get_calc(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Результат: {sum_polynom(data, data1)}')
 
 
-my_token = "5807651404:AAHL2ysrkFqlODCmk_snxneYfKQtizr3D1g"
+my_token = "input token here"
 app = ApplicationBuilder().token(my_token).build()
 app.add_handler(CommandHandler("start", hello))
 app.add_handler(CommandHandler("calc", get_calc))
